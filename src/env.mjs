7ea90e08a3c017cb9b845a1840e0521c9726aa23
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2023-02-21 14:20:48 +0200

    changed variables back

--- src/env.mjs
+++ src/env.mjs
@@ -19,10 +19,9 @@ const server = z.object({
     process.env.VERCEL ? z.string().min(1) : z.string().url(),
   ),
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-  GITHUB_CLIENT_CLIENT_ID: z.string(),
-  GITHUB_CLIENT_CLIENT_SECRET: z.string(),
-  GITHUB_PROD_CLIENT_ID: z.string(),
-  GITHUB_PROD_CLIENT_SECRET: z.string(),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
+
 });
 
 /**
@@ -44,10 +43,8 @@ const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  GITHUB_PROD_CLIENT_ID: process.env.GITHUB_PROD_CLIENT_ID,
-  GITHUB_PROD_CLIENT_SECRET: process.env.GITHUB_PROD_CLIENT_SECRET,
-  GITHUB_CLIENT_CLIENT_ID: process.env.GITHUB_CLIENT_CLIENT_ID,
-  GITHUB_CLIENT_CLIENT_SECRET: process.env.GITHUB_CLIENT_CLIENT_SECRET
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
